312fc96838e4545d7c3e23215a89d4e7c930ba72
rizyaking <168154645+rizyaking@users.noreply.github.com>
2024-04-29 15:56:24 +0900

    Add files via upload
    カメラを対象のオブジェクトを中心としてx軸回転できるようにしました。

---
+++ My project/Assets/cammeraControlle.cs
@@ -0,0 +1,42 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    GameObject mainCamera;
+    GameObject fieldObject;
+    public float rotateSpeed = 1.0f;
+    private Vector3 lastMousePosition; //‡@’Ç‹L
+    private Vector3 newAngle = new Vector3(0, 0, 0); //‡A’Ç‹L
+    void Start()
+    {
+        this.mainCamera = Camera.main.gameObject;
+        this.fieldObject = GameObject.Find("plate");
+    }
+    void Update()
+    {
+        //’Ç‹L START
+        if (Input.GetMouseButtonDown(0)) //‡B
+        {
+            newAngle = mainCamera.transform.localEulerAngles; //‡C
+            lastMousePosition = Input.mousePosition; //‡D
+        }
+        //’Ç‹L END
+        else if (Input.GetMouseButton(0))
+        {
+            rotateCamera();
+        }
+    }
+
+    private void rotateCamera()
+    {
+        Vector3 angle = new Vector3(
+                Input.GetAxis("Mouse X") * this.rotateSpeed,
+                0,
+                0
+            );
+        this.mainCamera.transform.RotateAround(this.fieldObject.transform.position, Vector3.up, angle.x);
+    }
+}
